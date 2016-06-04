f121f5430ed74b4261d6cca7a95e5cd4af365e6d
Marcus Vinicius <vmvini@hotmail.com>
2016-06-04 17:30:28 -0300

    tomcat replaced by glassfish for web module

--- PeopleCrud/src/main/java/com/vmvini/peoplecrud/PersonController.java
+++ PeopleCrud/src/main/java/com/vmvini/peoplecrud/PersonController.java
@@ -9,37 +9,33 @@ import com.vmvini.peoplecrudlibrary.Person;
 import com.vmvini.peoplecrudlibrary.PersonService;
 import com.vmvini.peoplecrudlibrary.ServiceLocator;
 import java.util.List;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
 
 /**
  *
  * @author marcusviniv
  */
 
-@ManagedBean
+@Named
 @RequestScoped
 public class PersonController {
     
     private Person person = new Person();
     private ServiceLocator sl = new ServiceLocator();
     
-    //@EJB(lookup="java:global/PeopleCrudCore/PersonServiceImpl")
+    //@EJB(lookup="corbaname:iiop:pcCore:370#java:global/PeopleCrudCore/PersonServiceImpl!com.vmvini.peoplecrudlibrary.PersonService")
     private PersonService personService;
     
-    private PersonService getPersonService(){
-        personService = sl.lookup("java:global/PeopleCrudCore/PersonServiceImpl", PersonService.class);
-        return personService;
-    }
-    
+   
     public void addPerson(){
-        personService = getPersonService();
+        personService = sl.lookup("java:global/PeopleCrudCore/PersonServiceImpl!com.vmvini.peoplecrudlibrary.PersonService", PersonService.class);
         personService.save(person);
         person = new Person();
     }
     
     public List<Person> listPersons(){
-        personService = getPersonService();
         return personService.list();
     }
     
