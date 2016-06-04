cd6f4d47d6957bb6861a2307fd8d16fa3b9f8015
Marcus Vinicius <vmvini@hotmail.com>
2016-06-04 17:32:11 -0300

    glassfish pcCore link

--- PeopleCrudLibrary/src/main/java/com/vmvini/peoplecrudlibrary/ServiceLocator.java
+++ PeopleCrudLibrary/src/main/java/com/vmvini/peoplecrudlibrary/ServiceLocator.java
@@ -22,8 +22,12 @@ public class ServiceLocator {
             Properties props = new Properties();
             props.put(Context.INITIAL_CONTEXT_FACTORY,
                     "com.sun.enterprise.naming.SerialInitContextFactory");
-            props.setProperty("org.omg.CORBA.ORBInitialHost", "localhost");
-            props.setProperty("org.omg.CORBA.ORBInitialPort", "370");
+            props.setProperty("org.omg.CORBA.ORBInitialHost", "pcCore");
+            props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
+            props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
+            props.setProperty(Context.STATE_FACTORIES, "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+            props.setProperty(Context.URL_PKG_PREFIXES, "com.sun.enterprise.naming");
+            
             InitialContext context = new InitialContext(props);
             return (T) context.lookup(recurso);
         } catch (NamingException ne) {
