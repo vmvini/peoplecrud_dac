3727682da864475c218d0a718f11b942b793793a
Marcus Vinicius <vmvini@hotmail.com>
2016-06-05 09:21:38 -0300

    adding reusability to ServiceLocator

--- PeopleCrudLibrary/src/main/java/com/vmvini/peoplecrudlibrary/ServiceLocator.java
+++ PeopleCrudLibrary/src/main/java/com/vmvini/peoplecrudlibrary/ServiceLocator.java
@@ -14,16 +14,28 @@ import javax.naming.NamingException;
 
 /**
  *
- * @author Ricardo Job
+ * @author marcusviniv
  */
 public class ServiceLocator {
+    
+    private String host = "pcCore";
+    private String port = "3700";
+    
+    public void setHost(String host){
+        this.host = host;
+    }
+    
+    public void setPort(String port){
+        this.port = port;
+    }
+    
     public <T> T lookup(String recurso, Class<T> tipo) {
         try {
             Properties props = new Properties();
             props.put(Context.INITIAL_CONTEXT_FACTORY,
                     "com.sun.enterprise.naming.SerialInitContextFactory");
-            props.setProperty("org.omg.CORBA.ORBInitialHost", "pcCore");
-            props.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
+            props.setProperty("org.omg.CORBA.ORBInitialHost", host);
+            props.setProperty("org.omg.CORBA.ORBInitialPort", port);
             props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "com.sun.enterprise.naming.SerialInitContextFactory");
             props.setProperty(Context.STATE_FACTORIES, "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
             props.setProperty(Context.URL_PKG_PREFIXES, "com.sun.enterprise.naming");
